00e228fa4b4d73293139c455765779a6df3c87ee
Cristofer Flores <cristofer.flort@gmail.com>
2017-10-05 18:36:02 -0500

    Added the configuration on development env to use PostgreSQL

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,22 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    // client: 'sqlite3',
+    // connection: {
+    //   filename: './dev.sqlite3'
+    // }
+    client: 'postgresql',
     connection: {
-      filename: './dev.sqlite3'
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   },
 
