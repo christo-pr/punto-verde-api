7643678e4b6b197ac7f45b78105f47fbf0cc7f7b
Cristofer Flores <cristofer.flort@gmail.com>
2017-10-05 18:53:31 -0500

    Created the sector migration and add some setup connection for the dev enviroment

--- knexfile.js
+++ knexfile.js
@@ -8,17 +8,18 @@ module.exports = {
     //   filename: './dev.sqlite3'
     // }
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    // connection: {
+    //   database: process.env.,
+    //   user:     'username',
+    //   password: 'password'
+    // },
+    connection: process.env.PG_CONNECTION_STRING || 'postgres://ksvnxvwc:07ZxD-XY6ymz-Idki-Vu5scwkYlLePYm@stampy.db.elephantsql.com:5432/ksvnxvwc',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'migrations'
     }
   },
 
--- migrations/20171005183448_sector.js
+++ migrations/20171005183448_sector.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
+	return Promise.all([
+	  knex.schema.createTable('sectors', function (table) {
+		  table.increments();
+		  table.uuid('uuid');
+		  table.string('name');
+		  table.timestamps();
+		})
+	]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+  	knex.schema.dropTableIfExists('sectors')
+  ])
 };
