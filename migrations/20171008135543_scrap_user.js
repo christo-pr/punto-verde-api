8fa4c86471bb67084190119fe3ef13c18a433c7b
Cristofer Flores <cristofer.flort@gmail.com>
2017-10-08 15:42:13 -0500

    Renamed columns on the join table

--- migrations/20171008135543_scrap_user.js
+++ migrations/20171008135543_scrap_user.js
@@ -5,9 +5,9 @@ exports.up = function(knex, Promise) {
 		  table.string('uuid').primary();
 		  table.specificType('id','serial');
 		  table.decimal('scrap_kg');
-		  table.string('sector_uuid').unsigned();
+		  table.string('scrap_uuid').unsigned();
 		  table.string('user_uuid').unsigned();
-		  table.foreign('sector_uuid').references('sectors.uuid');
+		  table.foreign('scrap_uuid').references('scraps.uuid');
 		  table.foreign('user_uuid').references('users.uuid');
 		  table.timestamps();
 		})
