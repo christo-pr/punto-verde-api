e412aad6606469c74a97a3483d1f1900d068a368
Cristofer Flores <cristofer.flort@gmail.com>
2017-10-08 12:29:53 -0500

    Created the scrap migration file

---
+++ migrations/20171008122526_scrap.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+	  knex.schema.createTable('scraps', function (table) {
+		  table.string('uuid').primary();
+		  table.specificType('id','serial');
+		  table.string('name');
+		  table.decimal('value_per_kg');
+		  table.timestamps();
+		})
+	]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+  	knex.schema.dropTableIfExists('scraps')
+  ])
+};
