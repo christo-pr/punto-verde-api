d7d8655a4c1d958a0898c9008f47b75560f58c57
Cristofer Flores <cristofer.flort@gmail.com>
2017-10-10 23:10:48 -0500

    Setup the knex config file for development

--- knexfile.js
+++ knexfile.js
@@ -3,17 +3,13 @@
 module.exports = {
 
   development: {
-    // client: 'sqlite3',
-    // connection: {
-    //   filename: './dev.sqlite3'
-    // }
     client: 'postgresql',
-    // connection: {
-    //   database: process.env.,
-    //   user:     'username',
-    //   password: 'password'
-    // },
-    connection: process.env.PG_CONNECTION_STRING || 'postgres://efmfbmrwceargq:15cb3c5b1d0a7db7d078aa9e87a26ac7e0e9b5f740b55e223b0003effd6676b1@ec2-184-73-189-221.compute-1.amazonaws.com:5432/d2rpdh9kpdatq5?ssl=true',
+    connection: {
+      host: '127.0.0.1',
+      database: 'put-your-db',
+      user:     'put-your-user',
+      password: 'put-your-pass'
+    },
     pool: {
       min: 2,
       max: 10
